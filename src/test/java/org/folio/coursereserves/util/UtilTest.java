728f97023226957bd0685f6aa25b047b22c1c1ee
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2020-01-30 09:56:17 -0500

    Add new test package

---
+++ src/test/java/org/folio/coursereserves/util/UtilTest.java
@@ -0,0 +1,54 @@
+package org.folio.coursereserves.util;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.folio.rest.jaxrs.model.LocationObject;
+import org.folio.rest.jaxrs.model.TemporaryLocationObject;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class UtilTest {
+
+  @Test
+  public void testPojoFromJson() throws Exception {
+    LocationObject locationObject = new LocationObject();
+    String name = "Front Desk";
+    String id = UUID.randomUUID().toString();
+    JsonObject json = new JsonObject();
+    json.put("id", id);
+    json.put("name", name);
+    json.put("isActive", Boolean.TRUE);
+    JsonArray servicePointIds = new JsonArray();
+    servicePointIds.add(UUID.randomUUID().toString());
+    servicePointIds.add(UUID.randomUUID().toString());
+    servicePointIds.add(UUID.randomUUID().toString());
+    json.put("servicePointIds", servicePointIds);
+    List<PopulateMapping> mapList = new ArrayList<>();
+    mapList.add(new PopulateMapping("id"));
+    mapList.add(new PopulateMapping("name"));
+    mapList.add(new PopulateMapping("isActive", PopulateMapping.ImportType.BOOLEAN));
+    mapList.add(new PopulateMapping("servicePointIds", PopulateMapping.ImportType.STRINGLIST));
+    CRUtil.populatePojoFromJson(locationObject, json, mapList);
+    assertTrue(locationObject.getName().equals(name));
+    assertTrue(locationObject.getId().equals(id));
+    assertTrue(locationObject.getIsActive());
+    assertTrue(locationObject.getServicePointIds().size() == 3);
+  }
+
+  @Test
+  public void testCopyFields() {
+    TemporaryLocationObject tempLocationObject = new TemporaryLocationObject();
+    LocationObject locationObject = new LocationObject();
+    locationObject.setId(UUID.randomUUID().toString());
+    locationObject.setName("Big Library");
+    locationObject.setIsActive(Boolean.TRUE);
+    CRUtil.copyFields(tempLocationObject, locationObject);
+    assertTrue(tempLocationObject.getId().equals(locationObject.getId()));
+    assertTrue(tempLocationObject.getName().equals(locationObject.getName()));
+    assertTrue(tempLocationObject.getIsActive().equals(locationObject.getIsActive()));
+  }
+
+}
