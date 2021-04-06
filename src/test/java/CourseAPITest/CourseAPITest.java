9ebccd490933fb46eaed427bdc0bff1d520f67e2
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2021-04-06 07:49:35 -0400

    Re-enabled failing RMB test

--- src/test/java/CourseAPITest/CourseAPITest.java
+++ src/test/java/CourseAPITest/CourseAPITest.java
@@ -2694,7 +2694,7 @@ public class CourseAPITest {
    });
   }
   //This test won't pass due to RMB-585
-/*
+
   @Test
   public void testSearchReservesByCopyrightStatus(TestContext context) {
     Async async = context.async();
@@ -2723,7 +2723,7 @@ public class CourseAPITest {
 
     JsonObject reserve3Json = new JsonObject()
         .put("id", reserve3Id)
-        .put("itemId", OkapiMock.item2Id)
+        .put("itemId", OkapiMock.item3Id)
         .put("processingStatusId", PROCESSING_STATUS_2_ID)
         .put("temporaryLoanTypeId", OkapiMock.loanType1Id)
         .put("copyrightTracking", new JsonObject()
@@ -2746,7 +2746,7 @@ public class CourseAPITest {
          "?query=copyrightStatus.name==cc";
       return TestUtil.doRequest(vertx, getUrl, GET, standardHeaders, null, 200,
           "Get Reserves by Processing Status");
-   }).setHandler(res -> {
+   }).onComplete(res -> {
      if(res.failed()) {
        context.fail(res.cause());
      } else {
@@ -2759,7 +2759,7 @@ public class CourseAPITest {
      }
    });
   }
-  */
+  
 
    @Test
    public void testPutEmptyLocationIdToCourseListing(TestContext context) {
